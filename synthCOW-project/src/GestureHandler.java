49d4e38cfd109abf6354faddb5d9823e0c97b7fc
Markouka <mark.bestavros@live.com>
2015-04-11 16:23:14 -0400

    Add skeleton classes
    Complete full class names and inputs for GestureHandler

--- synthCOW-project/src/GestureHandler.java
+++ synthCOW-project/src/GestureHandler.java
@@ -1,6 +1,7 @@
 
 public class GestureHandler {
-	
+	//private SynthCOW cow = new SynthCOW;
+	//private int instrument;
 	
 	public GestureHandler(){
 		
@@ -12,6 +13,41 @@ public class GestureHandler {
 		}else{
 			
 		}
+	}
+	
+	public void changePitch(int pitch){
+		if(pitch < 0 || pitch > 4){
+			System.out.println("Pitch is out of bounds!");
+			return;
+		}
+		
+		if(pitch == 0){
+			//cow.playNote*****(a);
+		}else if(pitch == 1){
+			//cow.playNote*****(b);
+		}else if(pitch == 2){
+			
+		}else if(pitch == 3){
+			
+		}else if(pitch == 4){
+			
+		}
+	}
+	
+	public void changeVolume(int volume){
+		
+	}
+	
+	public void activateVolume(boolean on){
+		
+	}
+	
+	public void loop(){
+		
+	}
+	
+	public void record(){
+		
 	}
 	
 	
