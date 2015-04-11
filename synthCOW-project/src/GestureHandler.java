a858189770a539b689ce91c436ae9eb79153c088
Markouka <mark.bestavros@live.com>
2015-04-11 17:07:44 -0400

    Add documentation and value check
    Finish documentation and add check for legal volume values.

--- synthCOW-project/src/GestureHandler.java
+++ synthCOW-project/src/GestureHandler.java
@@ -34,20 +34,27 @@ public class GestureHandler {
 		}
 	}
 	
-	public void changeVolume(int volume){
+	public void changeVolume(double volume){
 		//calculate volume values to pass to cow; pure math
+		
+		if(volume < 0.0 || volume > 11){
+			System.out.println("Volume value is out of bounds!");
+			return;
+		}
+		
+		
 	}
 	
 	public void activateVolume(boolean on){
-		
+		//Used to activate sound
 	}
 	
 	public void loop(){
-		
+		//Loop sound from last recording start (either record() or loop()) and begin a new recording
 	}
 	
 	public void record(){
-		
+		//Similar to loop() but only used at beginning
 	}
 	
 	
