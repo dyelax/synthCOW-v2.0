80c2e2da4cabb7835ee7f219632a57ed1cb09cd5
Matt Cooper <matthew_cooper@brown.edu>
2015-04-11 13:48:14 -0400

    Added Sample.java

---
+++ synthCOW-project/src/Sample.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import com.leapmotion.leap.*;
+
+public class Sample {
+	public static void main(String[] args) {
+		Controller controller = new Controller();
+		
+        // Keep this process running until Enter is pressed
+        System.out.println("Press Enter to quit...");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+	
+	
+}
