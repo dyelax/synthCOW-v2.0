950114379f7ba831ec3334a2863d205e58f0d975
akarle <ajk715@gmail.com>
2015-04-11 16:28:27 -0400

    commitToRevert #swag

--- synthCOW-project/src/Sample.java
+++ synthCOW-project/src/Sample.java
@@ -1,14 +1,15 @@
 import java.io.IOException;
 import com.leapmotion.leap.*;
 
-public class Sample {
-	public static void main(String[] args) {
-		Controller controller = new Controller();
-		//Alex was here
-		//again
-		
-		//Mark was also here
-		
+class Sample {
+    public static void main(String[] args) {
+        // Create a sample listener and controller
+        SampleListener listener = new SampleListener();
+        Controller controller = new Controller();
+
+        // Have the sample listener receive events from the controller
+        controller.addListener(listener);
+
         // Keep this process running until Enter is pressed
         System.out.println("Press Enter to quit...");
         try {
@@ -16,7 +17,8 @@ public class Sample {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        // Remove the sample listener when done
+        controller.removeListener(listener);
     }
-	
-	
 }
