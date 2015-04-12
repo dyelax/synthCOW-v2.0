e3258bb662230c31a3eb9531e23318543964692f
Matt Cooper <matthew_cooper@brown.edu>
2015-04-11 21:56:36 -0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	synthCOW-project/bin/GestureHandler.class
    	synthCOW-project/bin/Synth.class

--- synthCOW-project/src/MainApplet.java
+++ synthCOW-project/src/MainApplet.java
@@ -1,6 +1,8 @@
+import com.leapmotion.leap.Controller;
 import java.applet.*;
 import java.awt.*;
 import java.awt.geom.Point2D;
+import java.io.IOException;
 
 public class MainApplet extends Applet{
     private Point2D.Double leftHand, rightHand;
@@ -12,11 +14,30 @@ public class MainApplet extends Applet{
 
          leftHand = new Point2D.Double(10, 10);
          rightHand = new Point2D.Double(200, 200);
+
+         //setup leapmotion
+         // Create a sample listener and controller
+         SynthListener listener = new SynthListener();
+         Controller controller = new Controller();
+
+         // Have the sample listener receive events from the controller
+         controller.addListener(listener);
+
+         // Keep this process running until Enter is pressed
+//         System.out.println("Press Enter to quit...");
+//         try {
+//             System.in.read();
+//         } catch (IOException e) {
+//             e.printStackTrace();
+//         }
+//         // Remove the sample listener when done
+//         controller.removeListener(listener);
      }
 
     public void paint(Graphics g){
         Graphics2D g2 = (Graphics2D)g;
 
+        //draw background
         setBackground(Color.blue);
 
         if(handsOnScreen) {
@@ -26,7 +47,7 @@ public class MainApplet extends Applet{
             g2.drawOval((int) leftHand.getX(), (int) leftHand.getY(), 100, 100);
 
             if (!volumeOn){
-                g2.setColor(new Color(1f, 1f, 1f, 0.5f));
+                g2.setColor(new Color(1f, 1f, 1f, 0.4f));
             }
             g2.drawOval((int) rightHand.getX(), (int) rightHand.getY(), 100, 100);
 
@@ -35,24 +56,32 @@ public class MainApplet extends Applet{
             g2.fillOval((int) leftHand.getX(), (int) leftHand.getY(), 100, 100);
 
             if (!volumeOn){
-                g2.setColor(new Color(1f, 1f, 1f, 0.25f));
+                g2.setColor(new Color(1f, 1f, 1f, 0.2f));
             }
             g2.fillOval((int) rightHand.getX(), (int) rightHand.getY(), 100, 100);
         }
     }
 
     public void setLeftHandPos(double x, double y){
+        System.out.println("L: ("+x+", "+y+")");
         leftHand.setLocation(x, y);
     }
     public void setRightHandPos(double x, double y){
+        System.out.println("R: ("+x+", "+y+")");
         rightHand.setLocation(x, y);
     }
 
     public void setHandsOnScreen(boolean onScreen){
+//        System.out.println("onScreen: "+ onScreen ? 1 : 0);
         handsOnScreen = onScreen;
     }
 
     public void setVolumeOn(boolean on){
         volumeOn = on;
+    }
+
+    ///If next == true, switch to the next instrument, else switch to the previous instrument
+    public void changeInstrument(boolean next){
+
     }
 }
